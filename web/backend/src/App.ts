3f1520746546396ff809f104dc8df0f38759873d
eetar1 <ekwan14@gmail.com>
2019-09-13 13:22:00 -0700

    Fix run and modules

--- web/backend/src/App.ts
+++ web/backend/src/App.ts
@@ -1,36 +1,22 @@
 import * as express from 'express'
 
 class App {
-    public express;
+    public express
 
-    constructor() {
-        this.express = express();
-        this.mountRoutes();
+    constructor () {
+        this.express = express()
+        this.mountRoutes()
     }
 
-    private mountRoutes(): void {
-        const router = express.Router();
+    private mountRoutes (): void {
+        const router = express.Router()
         router.get('/', (req, res) => {
             res.json({
                 message: 'Hello World!'
-            });
-        });
-        this.express.use('/', router);
+            })
+        })
+        this.express.use('/', router)
     }
 }
 
-export const start = () => {
-
-    let app = new App().express;
-    const port = process.env.PORT || 3000;
-
-    app.listen(port, (err) => {
-        if (err) {
-            return console.log(err)
-        }
-
-        return console.log(`server is listening on ${port}`)
-    })
-}
-
-export default start
+export default new App().express
