2f11e89a27e5f4f3ede4ff98b94672213935056e
eetar1 <ekwan14@gmail.com>
2019-10-02 13:08:43 -0700

    Remove email validation

--- web/backend/src/models/user.ts
+++ web/backend/src/models/user.ts
@@ -2,17 +2,12 @@ export class User {
 
     public static MIN_DISPLAYNAME_LEN: number = 4;
 
-    // loosely validates email and displayName must be at least 4 characters
+    //displayName must be at least 4 characters
     public static validUser(user: User): boolean {
-        if (user.displayName === undefined || user.displayName === null || user.displayName.length < this.MIN_DISPLAYNAME_LEN) {
-            console.log(user.displayName);
-            console.log('bas usr');
-            return false;
-        }
-        return this.emailEx.test(user.email);
+        return !(user.displayName === undefined || user.displayName === null || user.displayName.length < this.MIN_DISPLAYNAME_LEN);
+
     }
 
-    private static emailEx: RegExp = /[\w.\d]+@\w+\.\w{2,3}/;
     public displayName: string;
     public email: string;
     public _id: string;
