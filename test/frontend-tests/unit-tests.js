b9beb9dbbd01dd5092e6f357e1fc679daab3695d
jordan <jordansandberg1@gmail.com>
2019-10-18 10:04:40 -0700

    Milestone3-test-configuration
    improve directory structure of test/ and add functionality which allows backend and front end tests to be run independently

---
+++ test/frontend-tests/unit-tests.js
@@ -0,0 +1,11 @@
+var chai = require("chai")
+var assert = chai.assert
+var expect = chai.expect
+var should = chai.should()
+
+describe('Frontend Unit Tests', () => {
+    it('should run a tests', () => {
+        var tautology = true
+        assert.isTrue(tautology, 'the test was run')
+    })
+})
