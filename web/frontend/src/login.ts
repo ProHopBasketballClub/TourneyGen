2a62b61222515405ff0be51394d3a3de4f407743
Landon Marshall <landonmarshall@Landons-MacBook-Pro.local>
2019-10-19 20:28:24 -0700

    env things:

--- web/frontend/src/login.ts
+++ web/frontend/src/login.ts
@@ -1,8 +1,7 @@
-//import * as express from 'express';
-//import {Router} from 'express';
+// import * as express from 'express';
+// import {Router} from 'express';
 import * as env from '../env.js';
-console.log("working");
-
+console.log('working');
 
 function get_login() {
     const username = (document.getElementById('username') as HTMLInputElement).value;
@@ -12,8 +11,8 @@ function get_login() {
 
 function check_login(username) {
     console.log(env);
-    //const route = env.BACKEND_LOCATION+'/user?displayName=' + username;
-    //console.log(route);
+    // const route = env.BACKEND_LOCATION+'/user?displayName=' + username;
+    // console.log(route);
 }
 
 export default get_login;
