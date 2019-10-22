cb7a5c448fbcd4644701e7c0e7446442c633ab5b
landonmarshall1997@gmail.com <landonmarshall@Landons-MacBook-Pro.local>
2019-10-22 00:01:43 -0700

    started creating home page

--- web/frontend/src/app.ts
+++ web/frontend/src/app.ts
@@ -21,6 +21,7 @@ async function api_get_request(route: string) {
     // Make a get request to the server at route.
 
     let data: string = '';
+    let status_code;
     let APIResponse = new Promise((resolve, reject) => {
         http.get(route, (resp) => {
             // A chunk of data has been recieved.
@@ -31,6 +32,8 @@ async function api_get_request(route: string) {
             // The whole response has been received. return the data
             resp.on('end', () => {
                 APIResponse = JSON.parse(data);
+                status_code = resp.statusCode;
+                console.log(status_code);
                 resolve(APIResponse);
             });
 
@@ -38,8 +41,8 @@ async function api_get_request(route: string) {
             reject('Error: ' + err.message);
         });
     });
-
-    return await APIResponse;
+    console.log(status_code);
+    return await [APIResponse, status_code];
 }
 
 app.get('/', (req, res) => {
@@ -50,11 +53,14 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
-app.get('/submit_login', (req, res) => {
-    const username = req.query.username ? req.query.username : '';
+app.post('/login', async (req, res) => {
+    const username = req.body.username ? req.body.username : '';
     const route = 'http://' + (env as any).env.BACKEND_LOCATION + '/Api/user?displayName=' + username;
 
-    const user_object: any = api_get_request(route);
+    const api_response = await api_get_request(route);
+    const user_object: any = api_response[0];
+    const status_code = api_response[1];
+
     let id = '';
     let email = '';
     let name = '';
@@ -64,13 +70,18 @@ app.get('/submit_login', (req, res) => {
         id = user_object._id;
         email = user_object.email;
     }
-
+    console.log(status_code);
     // Attach some sutff to browser....
 
     // Send user to their home page.
     // GH-9 should handle this.
     res.redirect('/login');
 });
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
 
 app.listen(port,() => {
     return console.info(`Server is listening on port ${port}`);
