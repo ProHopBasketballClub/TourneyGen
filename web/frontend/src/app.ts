0418f574f4e88d756a4ad2d0c91c6dbe78393b41
jordan <jordansandberg1@gmail.com>
2019-10-22 12:54:19 -0700

    WIP

--- web/frontend/src/app.ts
+++ web/frontend/src/app.ts
@@ -42,45 +42,45 @@ async function api_get_request(route: string) {
     return await APIResponse;
 }
 
-async function api_post_request(route: string, path: string,  body: object) {
-    const data = JSON.stringify(body);
-    let response = '';
-    const options = {
-        hostname: route,
-        port: 80,
-        path,
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': Buffer.byteLength(data),
-        },
-    };
-    let APIResponse = new Promise((resolve, reject) => {
-        const req = http.request(options, (resp) => {
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                response += chunk;
-                console.log(chunk);
-            });
+// async function api_post_request(route: string, path: string,  body: object) {
+//     const data = JSON.stringify(body);
+//     let response = '';
+//     const options = {
+//         hostname: route,
+//         port: 80,
+//         path,
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded',
+//           'Content-Length': Buffer.byteLength(data),
+//         },
+//     };
+//     let APIResponse = new Promise((resolve, reject) => {
+//         const req = http.request(options, (resp) => {
+//             // A chunk of data has been recieved.
+//             resp.on('data', (chunk) => {
+//                 response += chunk;
+//                 console.log(chunk);
+//             });
 
-            // The whole response has been received. return the data
-            resp.on('end', () => {
-                APIResponse = JSON.parse(data);
-                resolve(APIResponse);
-            });
+//             // The whole response has been received. return the data
+//             resp.on('end', () => {
+//                 APIResponse = JSON.parse(data);
+//                 resolve(APIResponse);
+//             });
 
-        }).on('error', (err) => {
-            reject('Error: ' + err.message);
-        });
+//         }).on('error', (err) => {
+//             reject('Error: ' + err.message);
+//         });
 
-        req.write(data);
-        console.log(response);
-        console.log();
-        req.end;
-    });
+//         req.write(data);
+//         console.log(response);
+//         console.log();
+//         req.end;
+//     });
 
-    return await APIResponse;
-}
+//     return await APIResponse;
+// }
 
 app.get('/', (req, res) => {
     res.redirect('/login');
@@ -112,27 +112,27 @@ app.get('/submit_login', async (req, res) => {
     res.redirect('/login');
 });
 
-app.get('/signup', (req, res) => {
-    res.render('signup');
-});
+// app.get('/signup', (req, res) => {
+//     res.render('signup');
+// });
 
-app.post('/signup', async (req, res) => {
+// app.post('/signup', async (req, res) => {
 
-    const username = req.body.username ? req.body.username : '';
-    const email = req.body.email ? req.body.email : '';
-    const route = 'http://' + (env as any).env.BACKEND_LOCATION;
-    const path = '/Api/user';
-    const body = {
-        displayName : username,
-        email,
-    };
-    console.log(body);
-    const user_object: any = await api_post_request(route, path, body);
-    const id = '';
-    const name = '';
+//     const username = req.body.username ? req.body.username : '';
+//     const email = req.body.email ? req.body.email : '';
+//     const route = 'http://' + (env as any).env.BACKEND_LOCATION;
+//     const path = '/Api/user';
+//     const body = {
+//         displayName : username,
+//         email,
+//     };
+//     console.log(body);
+//     const user_object: any = await api_post_request(route, path, body);
+//     const id = '';
+//     const name = '';
 
-    res.redirect('/signup');
-});
+//     res.redirect('/signup');
+})
 
 app.listen(port,() => {
     return console.info(`Server is listening on port ${port}`);
