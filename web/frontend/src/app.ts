9cf380c42cc08f83b1a3c6437d73f76c83300fb0
Ross Alexandra <ross-alexandra@outlook.com>
2019-10-21 23:56:35 -0700

    GH-11 - Added base token stuff and removed async.

--- web/frontend/src/app.ts
+++ web/frontend/src/app.ts
@@ -5,23 +5,27 @@ import * as express from 'express';
 import * as http from 'http';
 import * as path from 'path';
 import * as env from '../env';
+import * as cookieParser from 'cookie-parser'
 
 const app = express();
 const DEFAULT_PORT = 3001;
 const port = (env as any).env.PORT || DEFAULT_PORT;
+const user_route = 'http://' + (env as any).env.BACKEND_LOCATION + '/Api/user?displayName=';
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser()); // NOTE: If security is being implemented, a secret can be passed here.
 
 // Define useful functions.
-async function api_get_request(route: string) {
-    // Make a get request to the server at route.
+function api_get_request(route: string, callback) {
 
-    let data: string = '';
-    let APIResponse = new Promise((resolve, reject) => {
+    try {
+        let data: string = '';
+        let APIResponse;
+
         http.get(route, (resp) => {
             // A chunk of data has been recieved.
             resp.on('data', (chunk) => {
@@ -31,19 +35,78 @@ async function api_get_request(route: string) {
             // The whole response has been received. return the data
             resp.on('end', () => {
                 APIResponse = JSON.parse(data);
-                resolve(APIResponse);
+                console.log("Sending this back to callback: " + Object.keys(data));
+                callback(APIResponse);
             });
 
         }).on('error', (err) => {
-            reject('Error: ' + err.message);
+            callback('Error: ' + err.message);
         });
-    });
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-    return await APIResponse;
+function async_convert(params, async_method, callback) {
+    // callback gets 1 parameter. This parameter is called data.
+
+    function runCallback(data) {
+        callback(data);
+    }
+
+    // Calls the async method with the passed parameters,
+    // and a callback. The callback should take one parameter.
+    // That callback method will be run synchronously.
+    async_method(...params, runCallback);
+  
+}
+
+function is_logged_in(cookies): boolean {
+    const route = user_route + cookies.tourneygen_user;
+    const user_object: any = api_get_request(route);
+    //console.log("route: " + route);
+    //console.log("user_object: " + user_object);
+
+    // Error checking for user_object.
+
+    const auth_token = generate_auth_token(user_object.id, user_object.email, user_object.name, cookies.tourneygen_srn);
+
+    return auth_token === cookies.tourneygen_auth;
+}
+
+function generate_auth_token(id: string, email: string, name: string, random: number) {
+    // Theoretically some security could be added here.
+    // for now just returning id. 
+    // Note, methods using this function *should*
+    // use it in a way disallowing reversabililty (ie checking
+    // necessary information against this.) That way if real
+    // security is implemented here it will be useable.
+    return id;
+}
+
+function create_cookie(cookie_name, cookie_value, res) {
+    // Adds a cookie to the browser based on the cookie_obj passed.
+    res.cookie(cookie_name, cookie_value, { maxAge: 900000, httpOnly: true });
+}
+
+function destory_cookie(cookie_name: string, cookies) {
+    // Removed the cookie from the browser with the passed name.
+
+    if (!cookies.cookie_name) {
+        return false;
+    }
+
+    cookies.set(cookie_name, { expires: Date.now() });
+    return true;
 }
 
 app.get('/', (req, res) => {
-    res.redirect('/login');
+    if (!is_logged_in(req.cookies)) {
+        res.redirect('/login');
+    }
+    else {
+        res.render('index');
+    }
 });
 
 app.get('/login', (req, res) => {
@@ -51,25 +114,34 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/submit_login', (req, res) => {
-    const username = req.query.username ? req.query.username : '';
-    const route = 'http://' + (env as any).env.BACKEND_LOCATION + '/Api/user?displayName=' + username;
 
-    const user_object: any = api_get_request(route);
-    let id = '';
-    let email = '';
-    let name = '';
-
-    if (user_object) {
-        name = user_object.displayName;
-        id = user_object._id;
-        email = user_object.email;
+    if (is_logged_in(req.cookies)) {
+        // return some kind of error.
     }
 
-    // Attach some sutff to browser....
+    const username = req.query.username ? req.query.username : '';
+    console.log("Username: " + username);
 
-    // Send user to their home page.
-    // GH-9 should handle this.
-    res.redirect('/login');
+    const route = user_route + username;
+
+    api_get_request(route, (user_object) => {
+
+        let id = user_object ? user_object._id : '';
+        let email = user_object ? user_object.email : '';
+        let name = user_object ? user_object.displayName : '';
+        let random_number = Math.random();
+    
+        // Attach a "security random number" (srn)
+        // the user's auth token, and their username
+        // to the browser.
+        create_cookie('tourneygen_srn', random_number, res);
+        create_cookie('tourneygen_auth', generate_auth_token(id, email, name, random_number), res);
+        create_cookie('tourneygen_user', name , res);
+    
+        // Send user to their home page.
+        // GH-9 should handle this.
+        res.redirect('/login');
+    });
 });
 
 app.listen(port,() => {
