f72061ccb48f40760b9916e8eb0c1afa71960131
Ross Alexandra <rossandus@hotmail.com>
2019-10-22 17:39:49 -0700

    GH-11 - Added unit tests and fixed bugs.

--- web/frontend/src/constants/routes.ts
+++ web/frontend/src/constants/routes.ts
@@ -1,11 +1,1 @@
 export const user_route = '/Api/user';
-
-export function generate_get_route(route, args) {
-    let get_route = route + '?';
-    for (const [key, value] of Object.entries(args)) {
-        get_route += key.toString() + '=' + value.toString() + '&';
-    }
-
-    // Don't return the last &.
-    return get_route.slice(0, -1);
-}
