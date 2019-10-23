0d245dd2a08b2cc0ff58ac4aaedab9d6349c1ea8
landonmarshall1997@gmail.com <landonmarshall1997@gmail.com>
2019-10-23 13:11:20 -0700

    Got post request somewhat working for register

--- web/frontend/src/app.ts
+++ web/frontend/src/app.ts
@@ -1,10 +1,12 @@
 import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
 import * as express from 'express';
+import * as HttpStatus from 'http-status-codes';
 import * as path from 'path';
 import * as env from '../env';
 import { user_route } from './constants/routes';
-import { api_get_request, create_cookie, generate_auth_token, generate_get_route, is_logged_in } from './helpers/routing';
+
+import { api_get_request, api_post_request, create_cookie, generate_auth_token, generate_get_route, is_logged_in } from './helpers/routing';
 
 const app = express();
 const DEFAULT_PORT = 3001;
@@ -32,46 +34,6 @@ app.get('/', (req, res) => {
     });
 });
 
-// async function api_post_request(route: string, path: string,  body: object) {
-//     const data = JSON.stringify(body);
-//     let response = '';
-//     const options = {
-//         hostname: route,
-//         port: 80,
-//         path,
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/x-www-form-urlencoded',
-//           'Content-Length': Buffer.byteLength(data),
-//         },
-//     };
-//     let APIResponse = new Promise((resolve, reject) => {
-//         const req = http.request(options, (resp) => {
-//             // A chunk of data has been recieved.
-//             resp.on('data', (chunk) => {
-//                 response += chunk;
-//                 console.log(chunk);
-//             });
-
-//             // The whole response has been received. return the data
-//             resp.on('end', () => {
-//                 APIResponse = JSON.parse(data);
-//                 resolve(APIResponse);
-//             });
-
-//         }).on('error', (err) => {
-//             reject('Error: ' + err.message);
-//         });
-
-//         req.write(data);
-//         console.log(response);
-//         console.log();
-//         req.end;
-//     });
-
-//     return await APIResponse;
-// }
-
 app.get('/', (req, res) => {
     res.redirect('/login');
 });
@@ -93,7 +55,7 @@ app.post('/login', (req, res) => {
         console.log('Submitting login request to route: ' + route);
 
         api_get_request(route, (user_object) => {
-
+            console.log(user_object);
             if (!user_object) {
                 // User wasn't valid.
                 // When possible, pass that info along.
@@ -132,12 +94,28 @@ app.post('/signup', async (req, res) => {
 
     const username = req.body.username ? req.body.username : '';
     const email = req.body.email ? req.body.email : '';
-    // const route = 'http://' + (env as any).env.BACKEND_LOCATION;
-    // const path = '/Api/user';
-    // const body = {
-    //     displayName : username,
-    //     email,
-    // };
+    const route =  (env as any).env.BACKEND_LOCATION;
+    const url_path = '/Api/user';
+    const body = {
+        displayName : username,
+        email,
+    };
+    api_post_request(route, url_path, body, (user_object) => {
+
+        console.log(user_object);
+        if (user_object) {
+            if (user_object.status_code === HttpStatus.OK) {
+                api_post_request('', '/login', body, (login_object) => {
+                    console.log('logged in user');
+                    console.log(login_object, body);
+                });
+            }
+        } else {
+            console.log('');
+        }
+
+    });
+
     // console.log(body);
     // const user_object: any = await api_post_request(route, path, body);
     // const id = '';
@@ -150,7 +128,7 @@ app.post('/signup', async (req, res) => {
     // else {
     //     display(error_message)
     // }
-    res.redirect('/login');
+
 });
 
 app.listen(port,() => {
