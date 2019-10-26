b5e1cd0a8fe20b2ad1b4e1aa6a9e422e0974bb75
Ross Alexandra <rossandus@hotmail.com>
2019-10-25 23:06:23 -0700

    GH-17 - Renamed from leagues_ to edit_ in leagues.ts

--- web/frontend/src/js/leagues.ts
+++ web/frontend/src/js/leagues.ts
@@ -1,23 +1,23 @@
 // TODO: Refactor: extract modal into its own
 // seperate element for pug.
-let leagues_modal: any = document.getElementById('edit-league-modal');
-let leagues_editButton: any = document.getElementById('edit-league');
-let leagues_cancelButton: any = document.getElementById('cancel-edit-league');
-let leagues_submitButton: any = document.getElementById('submit-edit-league');
-let leagues_obscurer: any = document.getElementById('leagues-content-obscure');
+let edit_modal: any = document.getElementById('edit-league-modal');
+let edit_editButton: any = document.getElementById('edit-league');
+let edit_cancelButton: any = document.getElementById('cancel-edit-league');
+let edit_submitButton: any = document.getElementById('submit-edit-league');
+let edit_obscurer: any = document.getElementById('leagues-content-obscure');
 
-let leages_showModal = () => {
-    leagues_modal.style.display = 'block';
-    leagues_obscurer.style.display = 'block';
+let edit_showModal = () => {
+    edit_modal.style.display = 'block';
+    edit_obscurer.style.display = 'block';
 };
 
-let leagues_hideModal = () => {
-    leagues_modal.style.display = 'none';
-    leagues_obscurer.style.display = 'none';
+let edit_hideModal = () => {
+    edit_modal.style.display = 'none';
+    edit_obscurer.style.display = 'none';
 };
 
-leagues_editButton.onclick = leages_showModal;
+edit_editButton.onclick = edit_showModal;
 
-leagues_obscurer.onclick = leagues_hideModal;
-leagues_cancelButton.onclick = leagues_hideModal;
-leagues_submitButton.onclick = leagues_hideModal;
+edit_obscurer.onclick = edit_hideModal;
+edit_cancelButton.onclick = edit_hideModal;
+edit_submitButton.onclick = edit_hideModal;
