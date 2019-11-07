5c707cacff234686f2ed382a906d88254ae5d66d
eetar1 <ekwan14@gmail.com>
2019-11-07 13:42:50 -0800

    Fixed names in controllers

--- web/backend/src/controllers/team-controller.ts
+++ web/backend/src/controllers/team-controller.ts
@@ -15,7 +15,7 @@ export class TeamController implements IController {
         }
         if ((await MongoDb.getById(this.table, req.query.id)).data === null) {
             res.statusCode = HttpStatus.NOT_FOUND;
-            res.json({error: 'You cannot delete a match that does not exist'});
+            res.json({error: 'You cannot delete a team that does not exist'});
             return;
         }
         if (await MongoDb.deleteById(this.table, req.query.id)) {
@@ -116,7 +116,7 @@ export class TeamController implements IController {
         }
         if ((await MongoDb.getById(this.table, req.query.id)).data === null) {
             res.statusCode = HttpStatus.NOT_FOUND;
-            res.json({error: 'You cannot update a match that does not exist'});
+            res.json({error: 'You cannot update a team that does not exist'});
             return;
         }
         if (await MongoDb.updateById(this.table, req.query.id, req.body)) {
