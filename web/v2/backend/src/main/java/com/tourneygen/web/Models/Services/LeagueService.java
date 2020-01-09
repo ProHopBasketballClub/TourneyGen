7d0072354a6e2a5571b60df9835cb59df3ec4c90
eetar1 <ekwan14@gmail.com>
2020-01-09 07:08:11 -0800

    league Service Cleanup

--- web/v2/backend/src/main/java/com/tourneygen/web/Models/Services/LeagueService.java
+++ web/v2/backend/src/main/java/com/tourneygen/web/Models/Services/LeagueService.java
@@ -1,7 +1,16 @@
 package com.tourneygen.web.Models.Services;
 
+import com.tourneygen.web.Models.DTOs.LeagueDTO;
+import com.tourneygen.web.Models.DTOs.LeagueUpdateDTO;
 import com.tourneygen.web.Models.League;
+import java.util.List;
 
 public interface LeagueService {
-  void deleteLeague(League league);
+  void deleteLeague(long Id);
+
+  League updateLeague(LeagueUpdateDTO leagueDTO);
+
+  LeagueDTO createLeague(LeagueDTO leagueDTO);
+
+  List<LeagueDTO> findLeagueList(long id);
 }
--- web/v2/backend/src/main/java/com/tourneygen/web/Models/Services/LeagueServiceImpl.java
+++ web/v2/backend/src/main/java/com/tourneygen/web/Models/Services/LeagueServiceImpl.java
@@ -1,9 +1,14 @@
 package com.tourneygen.web.Models.Services;
 
+import com.tourneygen.web.Models.DTOs.LeagueDTO;
+import com.tourneygen.web.Models.DTOs.LeagueUpdateDTO;
 import com.tourneygen.web.Models.League;
 import com.tourneygen.web.Models.Repositories.LeagueRepository;
 import com.tourneygen.web.Models.Repositories.UserRepository;
 import com.tourneygen.web.Models.User;
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,10 +25,51 @@ public class LeagueServiceImpl implements LeagueService {
   }
 
   @Override
-  public void deleteLeague(League league) {
+  public void deleteLeague(long id) {
+    League league =
+        leagueRepository
+            .findById(id)
+            .orElseThrow(
+                () -> new EntityNotFoundException("League with id " + id + " was not found"));
     User user = league.getOwner();
     user.getLeagues().remove(league);
     userRepository.save(user);
     leagueRepository.deleteById(league.getId());
+  }
+
+  @Override
+  public League updateLeague(LeagueUpdateDTO leagueDTO) {
+    League league =
+        leagueRepository
+            .findById(leagueDTO.getId())
+            .orElseThrow(
+                () ->
+                    new EntityNotFoundException(
+                        "League with id " + leagueDTO.getId() + " was not found"));
+    league.merge(leagueDTO, userRepository);
+    return leagueRepository.save(league);
+  }
+
+  @Override
+  public LeagueDTO createLeague(LeagueDTO leagueDTO) {
+    League league = new League();
+    league.create(leagueDTO, userRepository);
+    league = leagueRepository.save(league);
+    leagueDTO.setId(league.getId());
+    return leagueDTO;
+  }
+
+  @Override
+  public List<LeagueDTO> findLeagueList(long id) {
+    return id < 0
+        ? LeagueDTO.findAll(leagueRepository.findAll())
+        : Collections.singletonList(
+            new LeagueDTO(
+                leagueRepository
+                    .findById(id)
+                    .orElseThrow(
+                        () ->
+                            new EntityNotFoundException(
+                                "League with id " + id + " was not found"))));
   }
 }
