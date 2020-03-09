ae3c3726cb2a54557f8c9f93b4f8a623faa8232a
eetar1 <ekwan14@gmail.com>
2020-03-09 16:54:21 -0700

    Fix ignore

---
+++ web/v2/frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Login from "./components/Login/Login";
+import SignUp from "./components/Login/SignUp";
+import Home from "./components/Home/Home"
+
+function App() {
+    return (<BrowserRouter>
+            <div style={{backgroundColor: '#282c34'}}>
+                <div className={'auth-wrapper'}>
+                    <div className={'auth-inner'}>
+                        <Switch>
+                            <Route exact path={'/'} component={Home} />
+                            <Route path={'/sign-in'} component={Login}/>
+                            <Route path={'/sign-up'} component={SignUp} />
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
