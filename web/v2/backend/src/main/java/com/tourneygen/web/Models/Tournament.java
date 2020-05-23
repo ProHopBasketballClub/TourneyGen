09a66b7c45e97b4690f3e7f183f1500dffa2d480
eetar1 <ekwan14@gmail.com>
2020-05-23 09:20:43 -0700

    Add tournaments (#179)

--- web/v2/backend/src/main/java/com/tourneygen/web/Models/Tournament.java
+++ web/v2/backend/src/main/java/com/tourneygen/web/Models/Tournament.java
@@ -1,5 +1,7 @@
 package com.tourneygen.web.Models;
 
+import java.util.Date;
+import java.util.Set;
 import javax.persistence.*;
 
 @Entity
@@ -8,9 +10,47 @@ public class Tournament {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  @ManyToOne private League league;
+  @OneToMany private Set<Bracket> brackets;
+
+  private String roundTime;
+
+  private String roundResetTime;
+
+  private Date date;
 
   public Long getId() {
     return id;
+  }
+
+  public String getRoundResetTime() {
+    return roundResetTime;
+  }
+
+  public void setRoundResetTime(String roundResetTime) {
+    this.roundResetTime = roundResetTime;
+  }
+
+  public String getRoundTime() {
+    return roundTime;
+  }
+
+  public void setRoundTime(String roundTime) {
+    this.roundTime = roundTime;
+  }
+
+  public Set<Bracket> getBrackets() {
+    return brackets;
+  }
+
+  public void setBrackets(Set<Bracket> brackets) {
+    this.brackets = brackets;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
   }
 }
